b337c3e38c9451edbafb9d11412485e84c66d78f
xX-LulzSecC4t-Xx <93016443+xX-LulzSecC4t-Xx@users.noreply.github.com>
2022-11-01 22:44:48 +0100

    Base Script

---
+++ lib/C4tScripts/Spoofing.lua
@@ -0,0 +1,32 @@
+--  ____    ____   _                        __        __    _          ____  ____ 
+-- |_   \  /   _| (_)                      |  ]      [  |  (_)        |_  _||_  _|
+--   |   \/   |   __   _ .--.  ,--.    .--.| | .---.  | |  __   ,--.    \ \  / /  
+--   | |\  /| |  [  | [ `/'`\]`'_\ : / /'`\' |/ /__\\ | | [  | `'_\ :    > `' <   
+--  _| |_\/_| |_  | |  | |    // | |,| \__/  || \__., | |  | | // | |, _/ /'`\ \_ 
+-- |_____||_____|[___][___]   \'-;__/ '.__.;__]'.__.'[___][___]\'-;__/|____||____|   
+----------------------------------------------------------------------------------------------------------
+-- Spoofing for MiradeliaX.
+-- Warning: Change it like u want.
+-- Creater: xX-LulzSecC4t-Xx
+----------------------------------------------------------------------------------------------------------
+
+-- {Accounts}
+Acc_1 = {Name = "Name", RID = "RID"}
+Acc_2 = {Name = "Name", RID = "RID"}
+Acc_3 = {Name = "Name", RID = "RID"}
+Acc_4 = {Name = "Name", RID = "RID"}
+Acc_5 = {Name = "Name", RID = "RID"}
+Acc_6 = {Name = "Name", RID = "RID"}
+Acc_7 = {Name = "Name", RID = "RID"}
+Acc_8 = {Name = "Name", RID = "RID"}
+Acc_9 = {Name = "Name", RID = "RID"}
+Acc_10 = {Name = "Name", RID = "RID"}
+Acc_11 = {Name = "Name", RID = "RID"}
+Acc_12 = {Name = "Name", RID = "RID"}
+Acc_13 = {Name = "Name", RID = "RID"}
+Acc_14 = {Name = "Name", RID = "RID"}
+Acc_15 = {Name = "Name", RID = "RID"}
+
+-- {Crews}
+Crew_1 = {CrewID = "53171732", CrewTag = "Dev", CrewName = "Administration Rockstar", CrewMotto = "Session-Justice League", CrewBadge = "on"}
+Crew_2 = {CrewID = "53171732", CrewTag = "XSEX", CrewName = "E-Girl Crew :*", CrewMotto = "Be my Simp", CrewBadge = "on"}
